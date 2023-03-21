ed02f9e9c8780a25beaf4b796de18fec8b22e68d
gogamamulashvili <121447578+gogamamulashvili@users.noreply.github.com>
2023-03-22 00:21:45 +0400

    Added blueprint

--- calcuator.cpp
+++ calcuator.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <map>
+#include <string>
 
+using namespace std;
+
+
+int to_arabic(string roman){
+    // Romauls gadaikvans arabulshi
+    map<string, int> m{{"I", 1}, {"V", 5}, {"X", 10}, {"L", 50}, {"C", 100}};
+    // @TODO implement
+}
+
+string to_roman(int num){
+    // Arabuls gadaikvans romaulshi
+    // @TODO: implement
+}
 
 int main(){
+    int number_1 = to_arabic("XIX");
+    int number_2 = to_arabic("IX");
+    int result;
+
+    char calc_operator = '*';
+
+    if(calc_operator == '*'){
+        result = number_1 * number_2;
+    }else if(calc_operator == '+'){
+        result = number_1 + number_2;
+    }else if (calc_operator == '-'){
+        result = number_1 - number_2;
+    }else if (calc_operator == '/'){
+        result = number_1 / number_2;
+    }
+
+    string roman_result = to_roman(result);
+    cout << "Shedegi: " << roman_result << endl;
+
     return 0;
 }
